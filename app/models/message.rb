0bdc75f703c27f09991b9f0651d6217b0af19cab
Yurie Hayashi <yurie.hayashi095@gmail.com>
2017-02-02 09:38:20 +0000

    change message_params

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates :name , length: { maximum: 20 } , presence:true
     # 内容は必須かつ2文字以上30文字以下
     validates :body , length: { minimum: 2, maximum: 30 } , presence:true
+    # 年齢は0歳から150歳まで
+    validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 150 }
 end
